9bcc0726dcf3f31f2c76137c24b4ca3488668a63
ananthrk <ananthrk@a91664e0-148d-11df-9524-c76eb015ffed>
2010-02-08 12:03:52 +0000

    One more implementation for problem 3 without using in-built methods

--- trunk/ananthrk/p3.scm
+++ trunk/ananthrk/p3.scm
@@ -2,3 +2,14 @@
 (define (get-element arg k) 
     (if (and (list? arg) (> k 0) (<= k (length arg)))
         (list-ref arg (- k 1))))
+
+;find the kth element in the list without using the in-built list-ref procedure
+(define (get-element-2 arg k)
+  (if (or (null? arg) (not (list? arg)))
+      arg      
+      (if (= k 1)
+          (car arg)
+          (get-element-2 (cdr arg) (- k 1))
+          )
+      )
+  )
