a146b75d89f67092341f7e8ddd92962ffdab27e7
ananthrk <ananthrk@a91664e0-148d-11df-9524-c76eb015ffed>
2010-02-08 09:18:19 +0000

    Modified solution to handle non-list and empty list as argument

--- trunk/ananthrk/p1.scm
+++ trunk/ananthrk/p1.scm
@@ -1,5 +1,8 @@
-(define last-element
-    (lambda (arg)
-      (if (null? (cdr arg))
-          (car arg)
-          (last-element (cdr arg)))))
+;find the last element in the list
+(define (last-element arg)
+    (if (list? arg)
+        (if (null?  arg)
+            '()
+            (if (null? (cdr arg))
+                (car arg)
+                (last-element (cdr arg))))))
