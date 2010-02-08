82600eef3088902381d157e8ed9a4b9733d53106
menakas.menakas <menakas.menakas@a91664e0-148d-11df-9524-c76eb015ffed>
2010-02-08 09:48:16 +0000

    Some minor changes

--- trunk/menakas/p2.scm
+++ trunk/menakas/p2.scm
@@ -1,7 +1,7 @@
 ; Find the last but one element of a list
 (define (my-penult lst)
 	(if (or (null? lst) (null? (cdr lst)))
-		null
+		'()
 		(if (null? (cddr lst))
 			(car lst)
 			(my-penult (cdr lst))
